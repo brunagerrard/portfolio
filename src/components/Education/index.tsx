a28d2a757a6c0d28f734ce3cb1242a235dff97ef
Bruna Nolasco <brunamnolasco@gmail.com>
2020-09-01 18:31:22 -0300

    starting education

---
+++ src/components/Education/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './education-styles.css';
+
+function Education() {
+  return (
+    <div className="education">
+      <h3>academia</h3>
+      <h4>MBA, Web Development - 2021 - IGTI</h4>
+      <h4>BA, Performing Arts - 2016 - UFBA</h4>
+    </div>
+  )
+}
+
+export default Education;
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 
 // import './about-styles.css';
 import Recommendations from '../../components/Recommendations';
+import { Link } from 'react-router-dom';
+import Education from '../../components/Education';
 
 function About() {
   return (
     <section>
+      <h2>education</h2>
+      <Education />
+      
       <h2>recommendations</h2>
       <Recommendations />
+
+      <Link to="/">home</Link>
     </section>
   )
 }
